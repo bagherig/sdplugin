a24d5f93e4fadde4a9b9ceb87ea5e2f721de96a5
Moeen Bagheri <moinbag@gmail.com>
2023-09-16 16:06:45 -0400

    fix 1

--- src/com.elgato.template.sdPlugin/actions/template/property-inspector/inspector.js
+++ src/com.elgato.template.sdPlugin/actions/template/property-inspector/inspector.js
@@ -1,96 +1,38 @@
-/// <reference path="../../../libs/js/property-inspector.js" />
-/// <reference path="../../../libs/js/utils.js" />
+document.addEventListener("DOMContentLoaded", function() {
+    // Load current settings
+    loadSettings();
 
-$PI.onConnected((jsn) => {
-    const form = document.querySelector('#property-inspector');
-    const {actionInfo, appInfo, connection, messageType, port, uuid} = jsn;
-    const {payload, context} = actionInfo;
-    const {settings} = payload;
-
-    Utils.setFormValue(settings, form);
-
-    form.addEventListener(
-        'input',
-        Utils.debounce(150, () => {
-            const value = Utils.getFormValue(form);
-            $PI.setSettings(value);
-        })
-    );
+    // Add event listener for the save button
+    document.getElementById("saveSettings").addEventListener("click", saveSettings);
 });
 
-$PI.onDidReceiveGlobalSettings(({payload}) => {
-    console.log('onDidReceiveGlobalSettings', payload);
-})
+function loadSettings() {
+    // Retrieve settings using the Stream Deck SDK
+    const settings = $PI.getSettings();
 
-/**
- * Provide window level functions to use in the external window
- * (this can be removed if the external window is not used)
- */
-window.sendToInspector = (data) => {
-    console.log(data);
-};
-
-document.querySelector('#open-external').addEventListener('click', () => {
-    window.open('../../../external.html');
-});
-
-
-/** 
- * TABS
- * ----
- * 
- * This will make the tabs interactive:
- * - clicking on a tab will make it active
- * - clicking on a tab will show the corresponding content
- * - clicking on a tab will hide the content of all other tabs
- * - a tab must have the class "tab"
- * - a tab must have a data-target attribute that points to the id of the content
- * - the content must have the class "tab-content"
- * - the content must have an id that matches the data-target attribute of the tab
- * 
- *  <div class="tab selected" data-target="#tab1" title="Show some inputs">Inputs</div>
- *  <div class="tab" data-target="#tab2" title="Here's some text-areas">Text</div>
- * a complete tab-example can be found in the index.html
-   <div type="tabs" class="sdpi-item">
-      <div class="sdpi-item-label empty"></div>
-      <div class="tabs">
-        <div class="tab selected" data-target="#tab1" title="Show some inputs">Inputs</div>
-        <div class="tab" data-target="#tab2" title="Here's some text-areas">Text</div>
-      </div>
-    </div>
-    <hr class="tab-separator" />
- * You can use the code below to activate the tabs (`activateTabs` and `clickTab` are required)
- */
-
-function activateTabs(activeTab) {
-    const allTabs = Array.from(document.querySelectorAll('.tab'));
-    let activeTabEl = null;
-    allTabs.forEach((el, i) => {
-        el.onclick = () => clickTab(el);
-        if(el.dataset?.target === activeTab) {
-            activeTabEl = el;
-        }
-    });
-    if(activeTabEl) {
-        clickTab(activeTabEl);
-    } else if(allTabs.length) {
-        clickTab(allTabs[0]);
-    }
+    // Populate the input fields with the retrieved settings
+    document.getElementById("recurringEvents").value = settings.recurringEvents || "";
+    document.getElementById("specialEvents").value = settings.specialEvents || "";
+    document.getElementById("timeUnit").value = settings.timeUnit || "seconds";
+    document.getElementById("alertTime").value = settings.alertTime || 5;
+    document.getElementById("soundSelection").value = settings.soundSelection || "default";
 }
 
-function clickTab(clickedTab) {
-    const allTabs = Array.from(document.querySelectorAll('.tab'));
-    allTabs.forEach((el, i) => el.classList.remove('selected'));
-    clickedTab.classList.add('selected');
-    activeTab = clickedTab.dataset?.target;
-    allTabs.forEach((el, i) => {
-        if(el.dataset.target) {
-            const t = document.querySelector(el.dataset.target);
-            if(t) {
-                t.style.display = el == clickedTab ? 'block' : 'none';
-            }
-        }
+function saveSettings() {
+    // Get values from input fields
+    const recurringEvents = document.getElementById("recurringEvents").value;
+    const specialEvents = document.getElementById("specialEvents").value;
+    const timeUnit = document.getElementById("timeUnit").value;
+    const alertTime = document.getElementById("alertTime").value;
+    const soundSelection = document.getElementById("soundSelection").value;
+
+    // Save the settings using the Stream Deck SDK
+    $PI.setSettings({
+        recurringEvents: recurringEvents,
+        specialEvents: specialEvents,
+        timeUnit: timeUnit,
+        alertTime: alertTime,
+        soundSelection: soundSelection
     });
 }
 
-activateTabs();
--- src/com.elgato.template.sdPlugin/app.js
+++ src/com.elgato.template.sdPlugin/app.js
@@ -2,18 +2,160 @@
 /// <reference path="libs/js/stream-deck.js" />
 
 const myAction = new Action('com.elgato.template.action');
+const audioContext = new (window.AudioContext || window.webkitAudioContext)();
 
-/**
- * The first event fired when Stream Deck starts
- */
+let timerValue = 0;
+let timerRunning = false;
+let timerInterval = null;
+
+function startTimer() {
+    timerRunning = true;
+    timerInterval = setInterval(() => {
+        timerValue++;
+        updateMainButtonDisplay();
+    }, 1000);
+}
+
+function pauseTimer() {
+    timerRunning = false;
+    clearInterval(timerInterval);
+}
+
+function resumeTimer() {
+    startTimer();
+}
+
+function incrementTimer() {
+    timerValue++;
+    updateMainButtonDisplay();
+}
+
+function decrementTimer() {
+    if (timerValue > 0) {
+        timerValue--;
+        updateMainButtonDisplay();
+    }
+}
+
+function updateMainButtonDisplay() {
+    // Update the main button's display with the timer value in "MM:SS" format
+    const minutes = Math.floor(timerValue / 60);
+    const seconds = timerValue % 60;
+    const displayTime = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+
+    // TODO: Update the main button's display with displayTime
+
+    // Check for events
+    checkForEvents();
+}
+
+function checkForEvents() {
+    const settings = $SD.getSettings();
+
+    // Convert dictionaries from string to object
+    const recurringEvents = JSON.parse(settings.recurringEvents || "{}");
+    const specialEvents = JSON.parse(settings.specialEvents || "{}");
+
+    const alertTime = settings.alertTime || 5;
+
+    // Check recurring events
+    for (const eventName in recurringEvents) {
+        const interval = settings.timeUnit === "minutes" ? recurringEvents[eventName] * 60 : recurringEvents[eventName];
+        if ((timerValue + alertTime) % interval === 0) {
+            triggerEvent(eventName);
+        }
+    }
+
+    // Check special events
+    for (const eventName in specialEvents) {
+        const eventTimes = settings.timeUnit === "minutes" ? specialEvents[eventName].map(t => t * 60) : specialEvents[eventName];
+        if (eventTimes.includes(timerValue + alertTime)) {
+            triggerEvent(eventName);
+        }
+    }
+}
+
+function triggerEvent(eventName) {
+    // Play the sound
+    const soundSelection = $SD.getSettings().soundSelection || "default";
+    const soundFilePath = `actions/template/assets/alert.mp3`; // Replace with the actual path to the sound files
+    playSound(soundFilePath);
+
+    // Display the icon
+    const iconFilePath = `actions/template/assets/bounty.jpg`; // Replace with the actual path to the icon files
+    displayIcon(iconFilePath);
+
+	// Add the event to the queue
+    eventQueue.push(eventName);
+
+    // If this is the only event in the queue, start displaying it
+    if (eventQueue.length === 1) {
+        displayNextEvent(iconFilePath);
+    }
+}
+
+function displayNextEvent() {
+    if (eventQueue.length === 0) return;
+
+    const currentEvent = eventQueue[0];
+
+    // Display the icon for the current event
+    const iconFilePath = `actions/template/assets/bounty.jpg`; // Replace with the actual path to the icon files
+    displayIcon(iconFilePath);
+
+    // Set a timeout to remove the current event from the queue and display the next one
+    setTimeout(() => {
+        eventQueue.shift();
+        if (eventQueue.length > 0) {
+            setTimeout(displayNextEvent, eventSwitchInterval);
+        }
+    }, eventDisplayDuration);
+}
+
+function playSound(soundFilePath) {
+    fetch(soundFilePath)
+        .then(response => response.arrayBuffer())
+        .then(data => audioContext.decodeAudioData(data))
+        .then(buffer => {
+            const source = audioContext.createBufferSource();
+            source.buffer = buffer;
+            source.connect(audioContext.destination);
+            source.start();
+        })
+        .catch(error => {
+            console.error("Error playing sound:", error);
+        });
+}
+
+function displayIcon(iconFilePath) {
+    // TODO: Use the Stream Deck SDK's setImage method to display the icon on the main button
+    $SD.setImage(iconFilePath);
+}
+
+
 $SD.onConnected(({ actionInfo, appInfo, connection, messageType, port, uuid }) => {
-	console.log('Stream Deck connected!');
+    console.log('Stream Deck connected!');
 });
 
 myAction.onKeyUp(({ action, context, device, event, payload }) => {
-	console.log('Your key code goes here!');
+    if (timerRunning) {
+        pauseTimer();
+    } else {
+        resumeTimer();
+    }
 });
 
 myAction.onDialRotate(({ action, context, device, event, payload }) => {
-	console.log('Your dial code goes here!');
+    console.log('Your dial code goes here!');
+});
+
+const incrementAction = new Action('com.elgato.increment');
+const decrementAction = new Action('com.elgato.decrement');
+
+incrementAction.onKeyUp(({ action, context, device, event, payload }) => {
+    incrementTimer();
+});
+
+decrementAction.onKeyUp(({ action, context, device, event, payload }) => {
+    decrementTimer();
 });
