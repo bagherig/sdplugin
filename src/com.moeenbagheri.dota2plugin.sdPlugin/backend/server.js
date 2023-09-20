dbf31f54d981c157d0a9f6257b447bcbdce4da94
Moeen Bagheri <moinbag@gmail.com>
2023-09-20 02:48:33 -0400

    fix

--- src/com.moeenbagheri.dota2plugin.sdPlugin/backend/server.js
+++ src/com.moeenbagheri.dota2plugin.sdPlugin/backend/server.js
@@ -2,32 +2,33 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const WebSocket = require('ws');
 
-const wss = new WebSocket.Server({ port: 8084 });
 const app = express();
+const PORT = 3000;
+const IP_ADDRESS = '0.0.0.0';  // Listen on all network interfaces
 
 // Middleware to parse JSON requests
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+// WebSocket setup
+const wss = new WebSocket.Server({ port: 8084 });
+
+wss.on('connection', (ws) => {
+    console.log('Client connected');
+});
 
 // Endpoint to receive data from Dota 2 GSI
-app.post('/gsi', (req, res) => {
+app.post('/', (req, res) => {
     console.log(req.body); // Log the game state data
     // TODO: Process the data and send it to the frontend
     res.sendStatus(200); // Send a success response
 });
 
-const GSI_PORT = 3000;
-app.listen(3000, () => {
-    console.log(`Server is running on port ${GSI_PORT}`);
+app.listen(PORT, IP_ADDRESS, () => {
+    console.log(`Server is running on ${IP_ADDRESS}:${PORT}`);
 });
 
-
-wss.on('connection', (ws) => {
-    console.log('Client connected');
-
-    // When receiving data from Dota 2 GSI
-    app.post('/gsi', (req, res) => {
-        console.log(req.body);
-        ws.send(JSON.stringify(req.body)); // Send the game state data to the frontend
-        res.sendStatus(200);
-    });
+app.use((req, res, next) => {
+    console.log(`Received request: ${req.method} ${req.url}`);
+    next();
 });
